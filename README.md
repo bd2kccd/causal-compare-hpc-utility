871a88f5216a143e75540bf0edea4637358c5ccb
yuanzhou <yuanzhou19@gmail.com>
2019-08-27 10:48:56 -0400

    Cleaned up code and added instructions

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # causal-compare-hpc-utility
 
+This is a utility tool written in Python to faciliate with running the [causal-compare](https://github.com/bd2kccd/causal-compare) CLI tool on HPC cluster. It generates individual configuration XML file based on the `causal-compare` configuration (multiple simulations and multiple algorithms with multiple combinations of parameters) so you can run the CLI tool for each single case and distribute the workload to the cluster nodes.
+
 ````
 cd causal-compare-hpc-utility
 python algoCompare.py
 ````
+
+The above run will generate a directory named as the original configuration file name and create individual XML configuration files in that directory. Then you can run the individual comparison with the CLI tool.
--- algoCompare.py
+++ algoCompare.py
@@ -2,19 +2,16 @@ import xml.etree.ElementTree as ET
 from itertools import product
 from xml.dom import minidom
 import copy
+import os
 from uuid import uuid4
 
-from pprint import pprint
-
-
-
-tree = ET.parse('comparison.xml')
+# Read in
+file = 'comparison.xml'
+filename = os.path.splitext(file)[0]
+tree = ET.parse(file)
 root = tree.getroot()
 
-pprint(root)
-pprint(root.tag)
-
-
+# Reuse the current comparison and statistics elements
 def copy_comparison():
     for comparison in root.iter('comparison'):
         return copy.deepcopy(comparison)
@@ -23,31 +20,35 @@ def copy_statistics():
     for statistics in root.iter('statistics'):
         return copy.deepcopy(statistics)
 
+# Get all unique combinations of parameters
+def get_parameters_combinations():
+    # Create a list of params lists
+    params_list = list()
+    for parameter in root.iter('parameter'):
+        values =  parameter.get('value')
+        # Split the comma-separated values into a list
+        values_list = values.split(',')
+        # Add the list to the params lists for calculating combinations
+        params_list.append(values_list)
 
-# Create a list of param lists
-params_list = list()
-for parameter in root.iter('parameter'):
-    print(parameter.tag, parameter.attrib)
-    values =  parameter.get('value')
-    values_list = values.split(',')
-    params_list.append(values_list)
+    # All combinations of a list of lists
+    params_combinations = list(product(*params_list))
+    return params_combinations
 
-# All combinations of a list of lists
-params_combinations = list(product(*params_list))
-
-
-def create_sims(simulation):
+# Create new simulations element for single config file
+def create_new_simulations(simulation):
     new_sims = ET.Element('simulations')
     new_sims.append(simulation)
     return new_sims
 
-def create_algos(algorithm):
+# Create new algorithms element for single config file
+def create_new_algorithms(algorithm):
     new_algos = ET.Element('algorithms')
     new_algos.append(algorithm)
     return new_algos
 
-# Create a parameters element for each parameters combination
-def create_params(combination):
+# Create a parameters element for each individual parameters combination
+def create_new_parameters(combination):
     parameters_element = root.find('parameters')
     parameters_list = parameters_element.findall('parameter')
     count = len(parameters_list)
@@ -62,48 +63,50 @@ def create_params(combination):
 
     return new_parameters
 
-
 # Return a pretty-printed XML string for the Element
-def prettify(elem):
-    rough_string = ET.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    # Use 4 spaces
-    return reparsed.toprettyxml()
+def prettify(element):
+    raw_string = ET.tostring(element, 'utf-8')
+    formatted_string = minidom.parseString(raw_string)
+    return formatted_string.toprettyxml()
 
 # Create single configuration xml file
 def create_single_config_file(simulations, algorithms, parameters):
-    root = ET.Element('comparisontool')
+    new_root = ET.Element('comparisontool')
     
     # Append new elements
-    root.append(simulations)
-    root.append(algorithms)
-    root.append(parameters)
+    new_root.append(simulations)
+    new_root.append(algorithms)
+    new_root.append(parameters)
 
     # Just copy over the old comparison and statistics
-    root.append(copy_comparison())
-    root.append(copy_statistics())
+    new_root.append(copy_comparison())
+    new_root.append(copy_statistics())
 
     # Pretty print the xml string 
-    formatted_str = prettify(root)
+    formatted_str = prettify(new_root)
 
     # Create the tree with the formatted string
-    tree = ET.ElementTree(ET.fromstring(formatted_str))
+    new_tree = ET.ElementTree(ET.fromstring(formatted_str))
     
     # Write to output file
     # Use a random UUID to name the file
-    tree.write(str(uuid4()) + "_config.xml", encoding='utf-8', xml_declaration=True, method="xml")
-
-
+    new_tree.write(str(uuid4()) + "_config.xml", encoding='utf-8', xml_declaration=True, method="xml")
 
 # Final output
-# Calculate the number of single config cases
-for simulation in root.iter('simulation'):
-    for algorithm in root.iter('algorithm'):
-        for params_combination in params_combinations:
-            new_simulations = create_sims(simulation)
-            new_algorithms = create_algos(algorithm)
-            new_parameters = create_params(params_combination)
+def output():
+    # Put all individual config files into a folder named with the orginal file name
+    os.mkdir(filename)
+    os.chdir(filename)
+    # Create a single config case each individual simulation, algorithm, and parameters combination
+    for simulation in root.iter('simulation'):
+        for algorithm in root.iter('algorithm'):
+            for params_combination in get_parameters_combinations():
+                new_simulations = create_new_simulations(simulation)
+                new_algorithms = create_new_algorithms(algorithm)
+                new_parameters = create_new_parameters(params_combination)
 
-            # Generating config file
-            create_single_config_file(new_simulations, new_algorithms, new_parameters)
+                # Generating config file
+                create_single_config_file(new_simulations, new_algorithms, new_parameters)
 
+# Entry point
+output()
